79294d9344134a0a890960e3f4546f05fd3903d9
bonnie <lovecry0424@gmail.com>
2024-06-15 14:28:22 +0800

    updata show and search

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 // 查詢文件位置
 const path = require('path')
+const restaurantsData = require('./restaurant.json').results
+
 
 const app = express()
 const port = 3000
@@ -15,7 +17,22 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index', { title })
+  res.render('index', { title, restaurantsData })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword ? req.query.keyword.toLowerCase() : '';
+  const filteredRestaurantsData = restaurantsData.filter(data => {
+    return data.name.toLowerCase().includes(keyword) || data.category.toLowerCase().includes(keyword);
+  });
+  res.render('index', { title, restaurantsData: filteredRestaurantsData, keyword });
+});
+
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurantId = req.params.restaurant_id
+  const data = restaurantsData.find(data => data.id.toString() === restaurantId)
+  res.render('show', { data })
 })
 
 
